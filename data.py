829b33fdfd9abb402f2583fc476a297dded5e9f5
Daniel CM <daniel_20589@hotmail.com>
2021-08-13 05:48:19 -0500

    Change in quiestions data.

---
+++ data.py
@@ -0,0 +1,123 @@
+question_data = [
+
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Hippopotomonstrosesquippedaliophobia is the irrational fear of long words.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "medium",
+        "question": "The Ceratosaurus, a dinosaur known for having a horn on the top of its nose, is also known to be a decendent of the Tyrannosaurus Rex.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Celebrities", "type": "boolean", "difficulty": "medium",
+        "question": "Michael Jackson had a pet python named &lsquo;Crusher&rsquo;.", "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+        "question": "Coca-Cola&#039;s original colour was green.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "The &quot;Gympie Stinger&quot; is the deadliest plant in the world.", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "medium",
+        "question": "The longest place named in the United States is Lake Chargoggagoggmanchauggagoggchaubunagungamaugg, located near Webster, MA.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+        "question": "The sum of all the numbers on a roulette wheel is 666.", "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Art", "type": "boolean", "difficulty": "medium",
+        "question": "Vincent van Gogh cut off one of his ears.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+        "question": "An eggplant is a vegetable.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "medium",
+        "question": "Gothenburg is the capital of Sweden.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+        "question": "The average woman is 5 inches \/ 13 centimeters shorter than the average man.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Mythology", "type": "boolean", "difficulty": "medium",
+        "question": "According to Norse mythology, Loki is a mother.", "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In Half-Life 2, if you play the zombies&#039; speech in reverse, they actually speak coherent English.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "History", "type": "boolean", "difficulty": "medium",
+        "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "History", "type": "boolean", "difficulty": "medium",
+        "question": "In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to raise moral questions about banning drugs.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "TF2: The Medic will be credited for an assist if he heals a Spy that successfully saps a building.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "medium",
+        "question": "&quot;Mongolia&quot; was a part of the now non-existent U.S.S.R.", "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Politics", "type": "boolean", "difficulty": "medium",
+        "question": "During the 2016 United States presidential election, the State of California possessed the most electoral votes, having 55.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In Monster Hunter Generations, guild style is a type of hunting style.", "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In the video game &quot;Transistor&quot;, &quot;Red&quot; is the name of the main character.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Television", "type": "boolean", "difficulty": "medium",
+        "question": "In &quot;Star Trek&quot;, Klingons are commonly referred to as &quot;Black Elves&quot;.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "medium",
+        "question": "In &quot;JoJo&#039;s Bizarre Adventure&quot;, Father Enrico Pucchi uses a total of 3 stands in Part 6: Stone Ocean.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "medium",
+        "question": "Finnish Lapphund dogs were used for herding reindeer.", "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Film", "type": "boolean", "difficulty": "medium",
+        "question": "The movie &quot;Tron&quot; received an Oscar nomination for Best Visual Effects.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In career mode of &quot;Need for Speed: Underground 2&quot;, the first car the player can drive is the BMW M3 GTR.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "During pre-production of &quot;Super Mario Bros.&quot;, Mario originally was meant to shoot bullets.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In the game &quot;Until Dawn&quot; Emily is the only playable character who can be killed by another playable character directly.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "A Boolean value of &quot;0&quot; represents which of these words?", "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Film", "type": "boolean", "difficulty": "medium",
+        "question": "The original ending of &quot;Little Shop Of Horrors&quot; has the plants taking over the world.",
+        "correct_answer": "True", "incorrect_answers": ["False"]}]
